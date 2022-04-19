d8a911b8e346c2d76f73e0025a362b651b3f5369
saifelleuch <sa2014if@gmail.com>
2022-04-19 07:38:23 -0700

    another one

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 #Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
-command => ['pkill killmenow'],
+command  => ['pkill -f killmenow'],
+provider => 'shell',
 }
