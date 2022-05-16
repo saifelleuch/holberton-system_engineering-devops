4bead493c6fb667dd59241a57a933798377df38a
saifelleuch <sa2014if@gmail.com>
2022-05-16 23:48:01 +0100

    another one

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+import requests
+from sys import argv
+if __name__ == '__main__':
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    finished_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            finished_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in finished_tasks))
