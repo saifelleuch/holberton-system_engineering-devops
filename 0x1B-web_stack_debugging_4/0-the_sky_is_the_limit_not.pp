e98564d2d5609b77b0d434acb3ae6237db7690df
saifelleuch <sa2014if@gmail.com>
2022-07-06 17:47:44 +0100

    another one

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fix number request failed 
+exec { 'fix number request failed':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
